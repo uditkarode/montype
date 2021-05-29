2c748fc96f251e83e79b4cf5f772bc8e71785ff5
Udit Karode <udit.karode@gmail.com>
2021-05-29 19:38:57 +0530

    Codegen: Getters: allow user to override ObjectProperty targets

--- src/Codegen/Codegen.hs
+++ src/Codegen/Codegen.hs
@@ -5,9 +5,11 @@ module Codegen.Codegen where
 import           Control.Monad   (foldM)
 import qualified Data.Map        as M
 import           Data.Text       as T (Text)
+import Data.Either ( fromRight )
 
 import           Codegen.Helpers (getTsType', myFoldM, tab)
 import           Parser.Property (Property (ObjectProperty, Property))
+import Config ( getMapped )
 
 -- takes a schema name and a list of properties,
 -- and returns the TypeScript interface for it
@@ -21,6 +23,6 @@ makeInterface name properties userTypes = props >>= \i -> pure $ start <> i <> e
           Property name descriptor -> do
             let tsDescrip = getTsType' descriptor 0 userTypes
             tsDescrip >>= \descrip -> pure $ acc <> tab <> name <> ": " <> descrip <> ";\n"
-          ObjectProperty name tsType -> Right $ acc <> tab <> name <> ": " <> tsType <> ";\n"
+          ObjectProperty name tsType -> Right $ acc <> tab <> name <> ": " <> fromRight tsType (getMapped tsType userTypes) <> ";\n"
           _ -> Right acc
     end = "};\n"
